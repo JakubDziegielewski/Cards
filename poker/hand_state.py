96fe0d45730cd4e01d2c53a779346b34304098eb
JakubDziegielewski <kubadziegiel1@gmail.com>
2025-07-14 12:36:27 +0200

    changed betting hsitory from list to str

--- poker/hand_state.py
+++ poker/hand_state.py
@@ -5,10 +5,10 @@ class HandState:
         self.current_bet = None
         self.pot = 0
         self.history = {
-            0: [],
-            1: [],
-            2: [],
-            3: [],
+            0: "",
+            1: "",
+            2: "",
+            3: "",
         }
         self.public_cards = None
     
@@ -16,4 +16,4 @@ class HandState:
         self.current_round += 1 
     
     def add_action(self, action_symbol: str) -> None:
-       self.history[self.current_round].append(action_symbol)
+       self.history[self.current_round] += action_symbol
